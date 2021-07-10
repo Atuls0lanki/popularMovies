dd1b125541da493dd70244d45c47d0f0a82118a3
Atuls0lanki <80041087+Atuls0lanki@users.noreply.github.com>
2021-07-10 19:49:05 +0530

    Updates

--- static/js/4.e1e8941b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmovie=this.webpackJsonpmovie||[]).push([[4],{197:function(t,e,a){"use strict";a.r(e);a(0);var i=a(38),n=a(74),c=a(5);e.default=function(){var t=Object(i.c)((function(t){return t.Movies})).datail;return Object(c.jsx)(n.a,{data:t})}}}]);
-//# sourceMappingURL=4.e1e8941b.chunk.js.map
